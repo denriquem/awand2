c942b04947338fcd80b1af3a5bf259f153bfdd55
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-03-09 20:19:48 +0100

    key set complete with validation

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,25 @@
+const KeyManager = require("../lib/KeyManager");
+const colors = require("colors");
+const inquirer = require("inquirer");
+const { isRequired } = require("../utils/validation");
+
 const key = {
-	set() {
-		console.log("Hello from set");
+	async set() {
+		const keyManager = new KeyManager();
+		const input = await inquirer.prompt([
+			{
+				type: "input",
+				name: "key",
+				message: "enter api key".green,
+				validate: isRequired,
+			},
+		]);
+
+		const key = keyManager.setKey(input.key);
+
+		if (key) {
+			console.log("API Key Set".blue);
+		}
 	},
 	show() {
 		console.log("Hello from show");
