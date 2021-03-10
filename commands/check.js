ae4a5c4d1e12fbc27aa1227244b28dc8f2ced7b2
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-03-10 20:48:29 +0100

    completed cryptoapi

--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,20 @@
+const KeyManager = require("../lib/KeyManager");
+const CrytpoApi = require("../lib/CryptoApi");
+
 const check = {
-	price(cmd) {
-		console.log(cmd.coin, cmd.curr);
+	async price(cmd) {
+		try {
+			keyManager = new KeyManager();
+			const key = keyManager.getKey();
+
+			const api = new CrytpoApi(key);
+
+			const priceOutputData = await api.getPriceData(cmd.coin, cmd.curr);
+
+			console.log(priceOutputData);
+		} catch (error) {
+			console.error(err.message.red);
+		}
 	},
 };
 
